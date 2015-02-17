a983b798115aba4d6b068126944d75149f2b4795
Md. Atiqul Islam <deadlineruhe@gmail.com>
2015-02-17 15:46:32 +0600

    Learing WPF initialize

---
+++ Learn.WPF/Learn.WPF/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Learn.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Applicaiton_StartUp(object s, StartupEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+
+            window.Title = "New Window";
+            if (e.Args.Length > 0)
+            {
+                MessageBox.Show("1st Args :" + e.Args[0]);
+            }
+
+            window.Show();
+        }
+    }
+}
---
+++ Learn.WPF/Learn.WPF/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Learn.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            Button btn = new Button()
+            {
+                Height = 50,
+                Width = 200
+            };
+            btn.FontWeight = FontWeights.Bold;
+
+            WrapPanel wpnl = new WrapPanel();
+
+            TextBlock txt = new TextBlock();
+            txt.Text = "Yellow";
+            txt.Foreground = Brushes.Yellow;
+
+            wpnl.Children.Add(txt);
+
+            txt = new TextBlock();
+
+            txt.Text = "Red";
+            txt.Foreground = Brushes.Red;
+            wpnl.Children.Add(txt);
+
+            txt = new TextBlock()
+            {
+                Text = "No Color"
+            };
+            wpnl.Children.Add(txt);
+
+            btn.Content = wpnl;
+            pnlMain.Children.Add(btn);
+
+            pnlMain.MouseDown += pnlMain_MouseUp;
+
+        }
+
+        private void pnlMain_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Clicked at position " + e.GetPosition(this).ToString());
+        }
+
+        private void Btn1_OnClick(object sender, RoutedEventArgs e)
+        {
+            lstBox.Items.Add(this.FindResource("Test").ToString());
+            lstBox.Items.Add(Application.Current.FindResource("GlobalResourceString").ToString());
+        }
+    }
+}
